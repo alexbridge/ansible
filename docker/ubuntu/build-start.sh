7dfd7b89ea4e47e067549854bd7a92bc08eb30cd
O.Bilenko <alexbridge.de@gmail.com>
2023-08-13 11:18:04 +0200

    Local docker port

--- docker/ubuntu/build-start.sh
+++ docker/ubuntu/build-start.sh
@@ -11,4 +11,4 @@ Utils::testType "docker"
 
 docker stop ansible-ubuntu && docker rm ansible-ubuntu
 docker build --tag ansible-ubuntu .
-docker run -p 23:22 --name ansible-ubuntu --privileged -it -d ansible-ubuntu
+docker run -p 22 --name ansible-ubuntu --privileged -it -d ansible-ubuntu
